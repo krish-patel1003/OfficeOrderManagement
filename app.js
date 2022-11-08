363017c015d15b35813b612d06139dcadc510822
Gohil Deepak <B61309@mail.jiit.ac.in>
2022-11-08 15:36:44 +0530

    MongoDB cluster created and app.js updated

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 const PORT = process.env.PORT || 3000
 
+dotenv.config()
 const app = express()
 
+const connect = async () => {
+    try {
+        console.log(process.env.PORT)
+        await mongoose.connect(process.env.MONGODB)
+        console.log('Connect to the database');
+    }
+    catch (err) {
+        throw err
+    }
+}
+
 app.get("/",(req,res)=>{
-    res.send("Hello")
+    res.send("hello")
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
+    connect()
     console.log("Server Started")
 })
